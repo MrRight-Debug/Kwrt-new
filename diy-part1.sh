8f942caf78722fed4dc0e7029306f16437b0ba13
MrRight-Debug <33514533+MrRight-Debug@users.noreply.github.com>
2025-07-26 11:58:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# 添加最新维护的插件源 (实时验证截至2025-07-26)
+echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >> feeds.conf.default
+echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall;luci' >> feeds.conf.default
+echo 'src-git mosdns https://github.com/sbwml/openwrt-mosdns' >> feeds.conf.default
+echo 'src-git openclash https://github.com/vernesong/OpenClash' >> feeds.conf.default
+echo 'src-git homeproxy https://github.com/immortalwrt/homeproxy' >> feeds.conf.default
+echo 'src-git adguardhome https://github.com/kongfl888/luci-app-adguardhome' >> feeds.conf.default
+echo 'src-git smartdns https://github.com/pymumu/smartdns' >> feeds.conf.default
+echo 'src-git zerotier https://github.com/mwarning/zerotier-openwrt' >> feeds.conf.default
+echo 'src-git argon https://github.com/jerrykuku/luci-theme-argon' >> feeds.conf.default
+echo 'src-git alist https://github.com/sbwml/luci-app-alist' >> feeds.conf.default
