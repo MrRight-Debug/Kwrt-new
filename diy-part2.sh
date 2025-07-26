7ca921290451a6235e96f5b92327550dc8a8a51a
MrRight-Debug <33514533+MrRight-Debug@users.noreply.github.com>
2025-07-26 11:54:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 修改默认IP地址
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# 修改默认主机名
+sed -i 's/OpenWrt/Kwrt/g' package/base-files/files/bin/config_generate
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# 设置alpha为默认主题
+sed -i 's/luci-theme-bootstrap/luci-theme-alpha/g' feeds/luci/collections/luci/Makefile
+cat << "EOF" > package/base-files/files/etc/uci-defaults/80-theme
+#!/bin/sh
+uci set luci.main.mediaurlbase='/luci-static/alpha'
+uci commit luci
+rm -rf /tmp/luci-*
+exit 0
+EOF
+chmod +x package/base-files/files/etc/uci-defaults/80-theme
