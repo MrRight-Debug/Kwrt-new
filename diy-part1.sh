e5cda3867de7c38d7e11598190195d9ecda15b77
MrRight-Debug <33514533+MrRight-Debug@users.noreply.github.com>
2025-07-26 12:37:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -4,5 +4,3 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >> feeds.conf.default
 echo 'src-git openclash https://github.com/vernesong/OpenClash' >> feeds.conf.default
-echo 'src-git zerotier https://github.com/mwarning/zerotier-openwrt' >> feeds.conf.default
-git clone --depth=1 https://github.com/destan19/openwrt-luci-theme-alpha.git package/luci-theme-alpha
