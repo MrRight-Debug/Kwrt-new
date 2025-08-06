e90ddc0d0c6c7df98bbb1553a06fb760adc94903
MrRight-Debug <33514533+MrRight-Debug@users.noreply.github.com>
2025-08-06 10:59:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,4 +3,3 @@
 # 添加最新维护的插件源 (实时验证截至2025-07-26)
 echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >> feeds.conf.default
-echo 'src-git openclash https://github.com/vernesong/OpenClash' >> feeds.conf.default
